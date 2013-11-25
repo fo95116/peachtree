90402b9e16325d1b18fe5f8973b8dae8ad75c682
fo95116 <fo95116@gmail.com>
2013-11-25 11:48:33 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peachtree::Application.config.secret_token = 'a8def3777da93273cd42d8acb1ce9523263df5b8a2f17e16ec25e31fc1221b86069b4cbd2b893037b55188250bfc77d023c10713dfb19f2c86d67b55b942aa67'
