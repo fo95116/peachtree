2e47602eb9975db8d9aeda64f837834df186d726
fo95116 <fo95116@gmail.com>
2013-11-26 09:22:33 -0800

    removed the wunderground gem for another approach to parsing

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -1,8 +1,5 @@
 class EnvironmentsController < ApplicationController
 
-  def index
-    w_api = Wunderground.new('441472960cf74c21')
-    wrapper_instance.[conditions]_and_[forecast]_for(san francisco)
-  end
+
 
 end
